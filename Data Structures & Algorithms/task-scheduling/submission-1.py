931544cb38087cbbd78499da69c8770943bbc0c3
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-04-01 08:02:37 -0400

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+       
+       count=Counter(tasks) # buil-in counter 
+       maxheap=[-n for n in count.values()] # making values -negative from the dict
+
+       heapq.heapify(maxheap)
+
+       q=deque() # this queue act as a process stack in js async cycle
+       cycle=0
+       while maxheap or q:
+        cycle +=1
+
+        if not maxheap: # just to make the process fast and empty the q fast not important
+            cycle =q[0][1]
+
+        else:
+            cnt=1 + heapq.heappop(maxheap)
+            if cnt:
+                next_process_time=cycle+n
+                q.append([cnt, next_process_time])
+
+        if q and q[0][1]==cycle:
+            heapq.heappush(maxheap, q.popleft()[0])
+
+       return cycle
+             
+
