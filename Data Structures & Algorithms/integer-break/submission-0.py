0726bddb97a339efee75e0089217a314342c3d90
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-04-23 09:04:30 -0400

    Add: integer-break - submission-0

---
+++ Data Structures & Algorithms/integer-break/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        maxvalue=0
+        mem={1:1}
+        def dfs(num):# the max product that we get after breaking num into positive integers >= 2
+
+            if num in mem:
+                return mem[num]
+            
+            maxvalue=0
+            for i in range(1, num):
+                maxvalue=max(maxvalue, (i*(num-i)) , i*dfs(num-i))
+                
+            mem[num]=maxvalue
+            return maxvalue
+
+        return dfs(n)
+
