726dc300ed7031635f3d509fc97585c3d43c9ff4
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-05-16 06:47:38 -0400

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        
+       mydict = {}
+
+       for i in range(len(order)):
+         mydict[order[i]] = i 
+       
+       compare_with=words[0]
+       for i in range(1, len(words)):
+        L = 0
+        R = 0
+
+        comparing = words[i]
+        while L < len(compare_with) and R < len(comparing):
+
+            if mydict[compare_with[L]] > mydict[comparing[R]]:
+                return False
+
+            elif mydict[compare_with[L]] == mydict[comparing[R]]:
+                L += 1
+                R += 1
+
+            else:
+                break
+
+        if R == len(comparing) and R < len(compare_with):
+            return False
+        compare_with = comparing
+            
+
+        
+       return True
+
+        
+
+
+       
+       
+
+
