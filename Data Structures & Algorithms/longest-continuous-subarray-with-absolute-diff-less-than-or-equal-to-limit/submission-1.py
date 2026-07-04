e9d2f1256cfa81a98ff4b6d2641d31f79f20e248
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-07-04 07:06:51 -0400

    Add: longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit - submission-1

---
+++ Data Structures & Algorithms/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        
+        '''
+        so I have an array 
+        i have to generate all the sumarray such that each element in the sub array is 
+        less than or equal to the limit.
+
+
+        '''
+
+        def dfs(i, arr):
+
+            if i >= len(nums):
+                return len(arr) - 1
+            
+            for j in range(len(arr)):
+                if abs(arr[j] - nums[i]) > limit:
+                    return len(arr)-1
+
+            arr.append(nums[i])
+            return dfs(i+1, arr )
+
+        ans = 0
+        for i in range(len(nums)):
+            ans = max(ans, dfs(i, [nums[i]]))
+
+        return ans
+
+
+
+                    
