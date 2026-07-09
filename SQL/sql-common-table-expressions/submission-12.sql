bf6d291cefeb8d2875b6077cffd0c7b44a440742
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-07-09 13:53:37 -0400

    Add: sql-common-table-expressions - submission-12

---
+++ SQL/sql-common-table-expressions/submission-12.sql
@@ -0,0 +1,46 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+
+with max_price AS
+
+(SELECT customer_id,max(price) AS highest_price
+FROM orders O
+GROUP BY customer_id
+HAVING max(price)<100) 
+
+
+SELECT c.name 
+FROM customers c
+JOIN max_price mp ON c.id = mp.customer_id
+ORDER BY c.name ASC;
+
