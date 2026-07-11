80c0ae11ad13eb369c527d03182d95e6881e9500
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-07-11 08:18:43 -0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        """
+        Brute force:
+        In that window limit we we need to find max all the time and return it in a list
+        using for loop.
+        pseudo code for that would be:
+        
+        leftp=0
+        Loop through the list with right pointer:
+
+            if k == rightp-leftp +1
+                maxvalue= max(nums[l:r+1])
+                l +=1
+                append maxvalue to the result
+        return result
+
+        This is O(n*k)
+
+        Now Can we do better what is our bottel neck ?
+        we had to calculate max of subarray all the time.
+        what can we do to reduce the time complexity of that?
+
+        I can think of the mootonic stack to find the max at each position
+
+        
+        """
+        result = []
+        l = 0
+        maxstack = deque()
+        for r in range(len(nums)):
+
+            while maxstack and maxstack[-1] < nums[r]:
+                maxstack.pop()
+            
+            maxstack.append(nums[r])
+
+            if k == r-l+1:
+                result.append(maxstack[0])
+                
+                if nums[l] == maxstack[0]:
+                    maxstack.popleft()
+                l +=1
+
+        return result
+
+
