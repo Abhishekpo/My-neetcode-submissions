f20c75a14460b61cd9610763ea58fa0a63a27302
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-07-28 08:29:05 -0400

    Add: unionFind - submission-5

---
+++ Data Structures & Algorithms/unionFind/submission-5.py
@@ -0,0 +1,45 @@
+class UnionFind:
+    
+    def __init__(self, n: int):
+        self.parent=[i for i in range(n)]
+        self.size =[0] * n
+        self.count = n
+        
+
+    def find(self, x: int) -> int:
+        p = self.parent[x]
+        if p != x:
+            self.parent[x]= self.find(self.parent[p])
+            p = self.parent[x]
+
+        return p
+        
+
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
+    def union(self, x: int, y: int) -> bool:
+        rootx= self.find(x)
+        rooty = self.find(y)
+
+        if rootx == rooty:
+            return False
+
+        self.count -= 1
+        
+        if self.size[rootx] < self.size[rooty]:
+            self.parent[x] = self.parent[y]
+            self.size[rooty] +=1
+            return True
+        self.parent[y] = self.parent[x]
+        self.size[rootx] +=1
+
+        return True
+        
+
+
+    def getNumComponents(self) -> int:
+        return self.count 
+
+
