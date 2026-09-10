a3ca1f9b8b7f2171b66a2ff0ebe14c5095da6474
Abhishek Poudel <98496284+Abhishekpo@users.noreply.github.com>
2026-09-10 10:02:21 -0400

    Add: binary-subarrays-with-sum - submission-1

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        """
+        Look at LiLO platform for the description.
+        """
+        L = 0
+        R = 0
+
+        currentSum =0
+        count =0
+        prefixzero =0
+        while R <len(nums):
+            currentSum +=nums[R]
+
+            while L < R and (nums[L] == 0 or currentSum > goal):
+
+                if nums[L] == 0:
+                    prefixzero +=1
+                else:
+                    prefixzero = 0
+
+                currentSum -=nums[L]
+                L+=1
+
+            if currentSum == goal:
+                count += 1 + prefixzero
+
+            R +=1
+           
+           
+        return count
